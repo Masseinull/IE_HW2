0bf2da49f563470a96258d696fd61669c9ec8f90
Masseinull <mha.sbu.ce@gmail.com>
2023-05-04 21:45:16 +0330

    add controller for people models

---
+++ controllers/student.controller.js
@@ -0,0 +1,55 @@
+const db = require("../models");
+const Student = db.student;
+
+exports.createStudent = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+    }
+
+    // Create a User
+    const student = new Student({
+        name: req.body.name,
+        _id: req.body.id,
+        password: req.body.password,
+        email: req.body.email,
+        phone: req.body.phone,
+        role: 'student',
+        study_level: req.body.study_level,
+        entry_year: req.body.entry_year,
+        entry_semester: req.body.entry_semester,
+        GPA: req.body.GPA,
+        faculty: req.body.faculty,
+        field: req.body.field
+    });
+
+    // Save student in the databases (user and student)
+    Student
+        .save(student)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the User."
+            });
+        });
+};
+
+exports.findAll = (req, res) => {
+    Student.find({})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving students."
+            });
+        });
+};
+
+
+
