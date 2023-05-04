b89ae5722edc08e588b3a302767fe7494edc3284
Masseinull <mha.sbu.ce@gmail.com>
2023-05-04 23:05:36 +0330

    edit schema defining in all models

--- models/teacher.model.js
+++ models/teacher.model.js
@@ -1,24 +1,21 @@
 const User = require('./user.model.js');
-const DiscriminatorOptions = require ('./user.model.js');
+const mongoose = require('mongoose');
+const Teacher = new mongoose.Schema({
+    faculty: {
+        type: String,
+        required: true
+    },
+    field:{
+        type: String,
+        required: true
+    },
+    level:{
+        type: String,
+        required: true,
+        enum: ['Lecturer', 'Associate Professor', 'Assistant Professor', 'Professor']
+    }
+}
+);
 
-module.exports = mongoose => {
-    const Teacher = User(mongoose).discriminator('teacher',
-        new mongoose.Schema({
-            faculty: {
-                type: String,
-                required: true
-            },
-            field:{
-                type: String,
-                required: true
-            },
-            level:{
-                type: String,
-                required: true,
-                enum: ['Lecturer', 'Associate Professor', 'Assistant Professor', 'Professor']
-            }
-
-        }, DiscriminatorOptions(DiscriminatorOptions))
-    );
-    return Teacher;
-};
+const teacher = User.discriminator('teacher', Teacher);
+module.exports = teacher;
--- models/user.model.js
+++ models/user.model.js
@@ -1,47 +1,45 @@
-const DiscriminatorOptions = { discriminatorKey: 'kind' };
-module.exports = DiscriminatorOptions;
-module.exports = mongoose => {
-    const schema = mongoose.Schema(
-        {
-            name: {
-                type: String,
-                required: true
-            },
-            _id: {
-                type: Number,
-                integer: true,
-                min: 0,
-                required: true
-            },
-            password: String,
-            email: {
-                type: String,
-                unique: true,
-                validate: {
-                    validator: function (value) {
-                        return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
-                    },
-                    message: props => `${props.value} is not a valid email address.`
-                }
-            },
-            phone: {
-                type: Number,
-                integer: true,
-                min: 0
-            },
-            role:{
-                type: String,
-                required: true,
-                enum: ['student', 'teacher', 'educational manager', 'admin']
-            }
-        }, DiscriminatorOptions
-    );
+const mongoose = require('mongoose');
 
-    schema.method("toJSON", function() {
-        const { __v, _id, ...object } = this.toObject();
-        object.id = _id;
-        return object;
-    });
-    const User = mongoose.model("user", schema);
-    return User;
-};
+const User = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        _id: {
+            type: Number,
+            integer: true,
+            min: 0,
+            required: true
+        },
+        password: String,
+        email: {
+            type: String,
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value);
+                },
+                message: props => `${props.value} is not a valid email address.`
+            }
+        },
+        phone: {
+            type: Number,
+            integer: true,
+            min: 0
+        },
+        role:{
+            type: String,
+            required: true,
+            enum: ['student', 'teacher', 'educational manager', 'admin']
+        }
+    }, { discriminatorKey: 'type' }
+);
+
+User.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+const user = mongoose.model("user", User);
+module.exports = user;
