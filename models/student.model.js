cac2c5257c6f8ebad07751a21ac4e5b062a93b29
Masseinull <mha.sbu.ce@gmail.com>
2023-05-04 18:29:05 +0330

    edit student model entry_semester

--- models/student.model.js
+++ models/student.model.js
@@ -14,9 +14,23 @@ module.exports = mongoose => {
                 min: 0
             },
             entry_semester: {
-                type: String,
-                enum: ['August', 'January'],
-                required: true
+                type: Number, // 4 digit number, XXX1 (August) , XXX2 (January), XXX3 (Summer)
+                required: true,
+                validate: {
+                    validator: function(value) {
+                        // Check if value is a 4-digit number
+                        if (value < 1000 || value > 9999) {
+                            return false;
+                        }
+                        // Check if last digit is 1, 2, or 3
+                        const lastDigit = value % 10;
+                        if (lastDigit !== 1 && lastDigit !== 2 && lastDigit !== 3) {
+                            return false;
+                        }
+                        return true;
+                    },
+                    message: 'Error: 406 (Invalid semester)',
+                }
             },
             GPA: {
               type: Number,
