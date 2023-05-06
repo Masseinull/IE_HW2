c7ea5ff85ea6acbcc16ad61b590d88c66756b3ed
Masseinull <mha.sbu.ce@gmail.com>
2023-05-06 13:52:08 +0330

    add manager controller

--- controllers/educational_manager.controller.js
+++ controllers/educational_manager.controller.js
@@ -1,50 +1,97 @@
-const db = require("../models");
-const Manager = db.educationalManager;
+const db = require("../models/index");
+const EducationalManager = db.educationalManager;
 
-
-
-exports.createTeacher = (req, res) => {
-    // Validate request
-    if (!req.body.title) {
-        res.status(400).send({ message: "Content can not be empty!" });
-        return;
-    }
-
-    // Create a User
-    const manager = new Manager({
+// Create a new educational manager
+exports.createEducationalManager = (req, res) => {
+    const educationalManager = new EducationalManager({
         name: req.body.name,
-        _id: req.body.id,
-        password: req.body.password,
+        _id: req.body._id,
         email: req.body.email,
+        password: req.body.password,
         phone: req.body.phone,
-        role: 'educational manager',
+        role: 'educationalManager',
         faculty: req.body.faculty
     });
 
-    // Save Manager in the databases (educational_manager and user)
-    Manager
-        .save(manager)
+    educationalManager.save()
         .then(data => {
             res.send(data);
         })
         .catch(err => {
             res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the teacher."
+                message: err.message || 'Some error occurred while creating the educational manager.'
             });
         });
 };
 
-// Find all managers
-exports.findAll = (req, res) => {
-    Manager.find({})
-        .then(data => {
-            res.send(data);
+// Retrieve all educational managers from the database
+exports.findAllEducationalManagers = (req, res) => {
+    EducationalManager.find()
+        .then(educationalManagers => {
+            res.send(educationalManagers);
         })
         .catch(err => {
             res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving managers."
+                message: err.message || 'Some error occurred while retrieving educational managers.'
             });
+        });
+};
+
+// Find a single educational manager with an educationalManagerId
+exports.findOneEducationalManager = (req, res) => {
+    const id = req.params.id;
+    EducationalManager.findById(id)
+        .then(educationalManager => {
+            if (educationalManager) {
+                res.status(200).json(educationalManager);
+            } else {
+                res.status(404).json({ message: 'Educational manager not found' });
+            }
+        })
+        .catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: 'Educational manager not found with id ' + id
+                });
+            }
+            return res.status(500).send({
+                message: 'Error retrieving educational manager with id ' + id
+            });
+        });
+};
+
+// Update an educational manager identified by the educationalManagerId in the request
+exports.updateEducationalManager = (req, res) => {
+    const id = req.params.id;
+    EducationalManager.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+        .then((educationalManager) => {
+            if (educationalManager) {
+                res.status(200).json(educationalManager);
+            } else {
+                res.status(404).json({ message: 'Educational manager not found' });
+            }
+        })
+        .catch((error) => {
+            if (error.name === 'ValidationError') {
+                res.status(400).json({ message: error.message });
+            } else {
+                res.status(500).json({ message: error.message });
+            }
+        });
+};
+
+// Delete an educational manager with the specified educationalManagerId in the request
+exports.deleteEducationalManager = (req, res) => {
+    const id = req.params.id;
+    EducationalManager.findByIdAndDelete(id)
+        .then((educationalManager) => {
+            if (educationalManager) {
+                res.status(200).json(educationalManager);
+            } else {
+                res.status(404).json({ message: 'Educational manager not found' });
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({ message: error.message });
         });
 };
