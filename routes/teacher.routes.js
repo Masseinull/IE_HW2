4a87e05470c3e1d158f32aac68765913c1731388
Masseinull <mha.sbu.ce@gmail.com>
2023-05-08 13:36:49 +0330

    re-set teacher routes

--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -3,22 +3,22 @@ module.exports = app => {
 
     var router = require("express").Router();
 
-    router.post("/admin/teacher", teacher.createTeacher);
+    router.post("/admin/Professor", teacher.createTeacher);
 
     // Retrieve all Teachers
-    router.get("/admin/teachers", teacher.findAllTeachers);
-    router.get("/teachers", teacher.findAllTeachers);
+    router.get("/admin/Professors", teacher.findAllTeachers);
+    router.get("/Professors", teacher.findAllTeachers);
 
     // Retrieve a single Teacher with id
-    router.get("/admin/teacher/:id", teacher.findOneTeacher);
-    router.get("/teacher/:id", teacher.findOneTeacher);
+    router.get("/admin/Professor/:id", teacher.findOneTeacher);
+    router.get("/Professor/:id", teacher.findOneTeacher);
 
     // Update a Teacher with id
-    router.put("/admin/teacher/:id", teacher.updateTeacher);
-    router.put("/teacher/:id", teacher.updateTeacher);
+    router.put("/admin/Professor/:id", teacher.updateTeacher);
+    router.put("/Professor/:id", teacher.updateTeacher);
 
     // Delete a Teacher with id
-    router.delete("/admin/teacher/:id", teacher.deleteTeacher);
+    router.delete("/admin/Professor/:id", teacher.deleteTeacher);
 
     app.use('/api', router);
 };
