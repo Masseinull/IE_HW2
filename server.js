5b61af578c8dfec90b16a98456869622c4883abc
Masseinull <mha.sbu.ce@gmail.com>
2023-05-11 20:26:49 +0330

    add a simple swagger.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json');
 const app = express();
 
 var corsOptions = {
@@ -14,7 +15,7 @@ app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 // simple route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Golestan API by Masseinull." });
