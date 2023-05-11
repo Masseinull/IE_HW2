7b3580571067f1b59e47c3b6e3e7e2ef36c21558
Masseinull <mha.sbu.ce@gmail.com>
2023-05-11 04:49:48 +0330

    add authenticator for professor updating info of himself/herself

---
+++ middleware/authTeacher.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+exports.isThisTeacher = async (req, res, next) => {
+    try {
+        // console.log(`role ${req.type}`);
+        if(req.type !== 'teacher'){
+            if(req.type === 'admin'){
+                return res.status(403).json({ error: 'admin have to request another route /admin/Professor' });
+            }
+            return res.status(403).json({ error: 'Access denied. You have to be professor to perform this action.' });
+        }
+        if(req.id !== req.params.id){
+            return res.status(403).json({ error: `Access denied. You have to be professor with id ${req.params.id} to perform this action. but you are professor id ${req.id}.` });
+        }
+        return next();
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+};
--- routes/teacher.routes.js
+++ routes/teacher.routes.js
@@ -1,24 +1,26 @@
 module.exports = app => {
     const teacher = require("../controllers/teacher.controller.js");
-
+    const { verifyToken } = require("../middleware/auth");
+    const { isAdmin } = require("../middleware/authAdmin");
+    const { isThisTeacher } = require("../middleware/authTeacher");
     var router = require("express").Router();
 
-    router.post("/admin/Professor", teacher.createTeacher);
+    router.post("/admin/Professor", verifyToken, isAdmin, teacher.createTeacher);
 
     // Retrieve all Teachers
-    router.get("/admin/Professors", teacher.findAllTeachers);
-    router.get("/Professors", teacher.findAllTeachers);
+    router.get("/admin/Professors", verifyToken, isAdmin, teacher.findAllTeachers);
+    router.get("/Professors", verifyToken, teacher.findAllTeachers);
 
     // Retrieve a single Teacher with id
-    router.get("/admin/Professor/:id", teacher.findOneTeacher);
-    router.get("/Professor/:id", teacher.findOneTeacher);
+    router.get("/admin/Professor/:id", verifyToken, isAdmin, teacher.findOneTeacher);
+    router.get("/Professor/:id", verifyToken, teacher.findOneTeacher);
 
     // Update a Teacher with id
-    router.put("/admin/Professor/:id", teacher.updateTeacher);
-    router.put("/Professor/:id", teacher.updateTeacher);
+    router.put("/admin/Professor/:id", verifyToken, isAdmin, teacher.updateTeacher);
+    router.put("/Professor/:id", verifyToken, isThisTeacher, teacher.updateTeacher);
 
     // Delete a Teacher with id
-    router.delete("/admin/Professor/:id", teacher.deleteTeacher);
+    router.delete("/admin/Professor/:id", verifyToken, isAdmin, teacher.deleteTeacher);
 
     app.use('/api', router);
 };
