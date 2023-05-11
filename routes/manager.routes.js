6022bcc351155b5e2feb3348b28ec354c0a39cef
Masseinull <mha.sbu.ce@gmail.com>
2023-05-11 04:53:52 +0330

    add authentication of admin inside manager routes

--- routes/manager.routes.js
+++ routes/manager.routes.js
@@ -1,22 +1,23 @@
 module.exports = app => {
     const educationalManager = require("../controllers/educational_manager.controller.js");
-
+    const {verifyToken} = require("../middleware/auth");
+    const {isAdmin} = require("../middleware/authAdmin");
     var router = require("express").Router();
 
     // Create a new Educational Manager
-    router.post("/admin/manager", educationalManager.createEducationalManager);
+    router.post("/admin/manager", verifyToken, isAdmin, educationalManager.createEducationalManager);
 
     // Retrieve all Educational Managers
-    router.get("/admin/managers", educationalManager.findAllEducationalManagers);
+    router.get("/admin/managers", verifyToken, isAdmin, educationalManager.findAllEducationalManagers);
 
     // Retrieve a single Educational Manager with id
-    router.get("/admin/manager/:id", educationalManager.findOneEducationalManager);
+    router.get("/admin/manager/:id", verifyToken, isAdmin, educationalManager.findOneEducationalManager);
 
     // Update an Educational Manager with id
-    router.put("/admin/manager/:id", educationalManager.updateEducationalManager);
+    router.put("/admin/manager/:id", verifyToken, isAdmin, educationalManager.updateEducationalManager);
 
     // Delete an Educational Manager with id
-    router.delete("/admin/manager/:id", educationalManager.deleteEducationalManager);
+    router.delete("/admin/manager/:id", verifyToken, isAdmin, educationalManager.deleteEducationalManager);
 
     app.use('/api', router);
 };
