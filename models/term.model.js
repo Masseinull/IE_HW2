3b5fda104a7d76d0692d8c8f253c07b40971dc10
atena motevallian <motevalliana99@gmail.com>
2023-06-25 17:05:01 +0330

    term_id and validators fixed

--- models/term.model.js
+++ models/term.model.js
@@ -3,9 +3,25 @@ const Semester_course = require('./semester_course.model')
 const mongoose = require('mongoose');
 
 const termSchema = new mongoose.Schema({
-    term_name: {
-        type: String ,
+    term_id: {
+        type: Number ,
         unique : true,
+        validate: {
+            validator: function(value) {
+                // Check if value is a 4-digit number
+                if (value < 1000 || value > 9999) {
+                    return false;
+                }
+                // Check if last digit is 1, 2, or 3
+                const lastDigit = value % 10;
+                if (lastDigit !== 1 && lastDigit !== 2 && lastDigit !== 3) {
+                    return false;
+                }
+                return true;
+            },
+            message: 'Error: 406 (Invalid term id)',
+        }
+    
     },
     users_id: {
             type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
