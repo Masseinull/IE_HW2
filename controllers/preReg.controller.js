293581446119c569d2630c81cdd1d908ee03ef38
Masseinull <mha.sbu.ce@gmail.com>
2023-06-25 18:09:30 +0330

    add retrieve term-based pre registration endpoint

--- controllers/preReg.controller.js
+++ controllers/preReg.controller.js
@@ -2,7 +2,25 @@ const Term = require('../models/teacher.model');
 const PreRegReq = require('../models/preRegistrationReq.model');
 
 exports.termBasedPreRegistrations = async (req, res) => {
-
+    const termId = req.params.id;
+    try {
+        PreRegReq.find({term_id: termId})
+            .then(preRegRequest => {
+                if (preRegRequest){
+                    res.status(200).send(preRegRequest);
+                }else{
+                    res.status(404).json({message: 'Pre registration requests not found in this term'});
+                }
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || 'Some error occurred while retrieving requests of pre registration.'
+                });
+            });
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Server error' });
+    }
 };
 
 exports.courseBasedPreRegistrations = async (req, res) => {
--- models/preRegistrationReq.model.js
+++ models/preRegistrationReq.model.js
@@ -2,6 +2,25 @@ const mongoose = require('mongoose');
 const semesterCourse = require('./semester_course.model')
 
 const preRegReqSchema = new mongoose.Schema({
+    term_id: {
+        type: Number,
+        ref: 'term',
+        validate: {
+            validator: function(value) {
+                // Check if value is a 4-digit number
+                if (value < 1000 || value > 9999) {
+                    return false;
+                }
+                // Check if last digit is 1, 2, or 3
+                const lastDigit = value % 10;
+                if (lastDigit !== 1 && lastDigit !== 2 && lastDigit !== 3) {
+                    return false;
+                }
+                return true;
+            },
+            message: 'Error: 406 (Invalid term id)',
+        }
+    },
     requesterId: {
         type: Number,
         ref: 'student'
