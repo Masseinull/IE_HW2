813ed3ec9df1865b2ebce304a2ba5f4a30c100b4
Masseinull <mha.sbu.ce@gmail.com>
2023-06-26 17:46:08 +0330

    add get and update for faculty

--- routes/faculty.routes.js
+++ routes/faculty.routes.js
@@ -1,10 +1,16 @@
 module.exports = app => {
     const faculty = require("../controllers/faculty.controller.js");
     const {isAdmin} = require("../middleware/authAdmin");
+    const {verifyToken} = require("../middleware/auth");
+
     var router = require("express").Router();
 
 
-    router.post("/admin/faculty", isAdmin, faculty.createFaculty);
+    router.post("/admin/faculty", verifyToken, isAdmin, faculty.createFaculty);
+
+    router.get("/admin/faculties", verifyToken, isAdmin, faculty.findAllFaculties);
+
+    router.put("/admin/faculty/:id", verifyToken, isAdmin, faculty.updateFaculty);
 
     app.use('/api', router);
 };
